12d93a0fd10c35397a275d9cd719ec7a5a47dfef
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-23 20:52:07 +0300

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+  username: {
+    type:String,
+    require: true,
+  }
+})
+
+let user = mongoose.model("User", UserSchema);
+module.exports = user;
